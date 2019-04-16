174537e17a158df61f414ebea51dae2e61decd42
kenasan <9zim@mail.ru>
2019-04-17 01:46:35 +0300

    overide toString

--- halloRepository/src/com/intexsoft/javacourse/tsimmerman/Main.java
+++ halloRepository/src/com/intexsoft/javacourse/tsimmerman/Main.java
@@ -1,12 +1,16 @@
 package com.intexsoft.javacourse.tsimmerman;
 
 public class Main {
-
     public static void main(String[] args) {
         Main.say();
     }
 
     private static void say() {
         System.out.println("Hello World!");
+    }
+
+    @Override
+    public String toString() {
+        return "Hallo Repository";
     }
 }
