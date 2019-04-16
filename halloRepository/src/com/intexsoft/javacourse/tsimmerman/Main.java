444c2e850ee6f6bdfea29735e53c8d1be281a563
kenasan <9zim@mail.ru>
2019-04-17 01:43:46 +0300

    create method say

--- halloRepository/src/com/intexsoft/javacourse/tsimmerman/Main.java
+++ halloRepository/src/com/intexsoft/javacourse/tsimmerman/Main.java
@@ -3,6 +3,10 @@ package com.intexsoft.javacourse.tsimmerman;
 public class Main {
 
     public static void main(String[] args) {
+        Main.say();
+    }
+
+    private static void say() {
         System.out.println("Hello World!");
     }
 }
