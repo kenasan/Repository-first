bbb0e9079ac8ad884edeccdccd36bb7fabc4b558
kenasan <9zim@mail.ru>
2019-04-17 02:03:15 +0300

    take hashCode of main

--- halloRepository/src/com/intexsoft/javacourse/tsimmerman/Main.java
+++ halloRepository/src/com/intexsoft/javacourse/tsimmerman/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         System.out.println(main.toString());
+        System.out.println(main.hashCode());
     }
 
     @Override
