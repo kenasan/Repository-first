0a971553e8bbca7268f7ec2d87bf3ba25acb8526
kenasan <9zim@mail.ru>
2019-04-18 12:15:30 +0300

    true .gitignore

--- halloRepository/src/com/intexsoft/javacourse/tsimmerman/Main.java
+++ halloRepository/src/com/intexsoft/javacourse/tsimmerman/Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Main main = new Main();
         System.out.println(main.toString());
-        System.out.println(main.hashCode());
-
     }
 
     @Override
